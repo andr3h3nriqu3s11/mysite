aac5546c0e81dbd65564f515c25a6d85d02c98da
Andre Henriques <andr3h3nriqu3s@gmail.com>
2021-10-06 19:12:02 +0100

    Vr 1.2

---
+++ src/components/HideShow.svelte
@@ -0,0 +1,31 @@
+<script>
+	let show = false;
+</script>
+
+<button
+	class="readmore"
+	on:click={() => {
+		show = !show;
+	}}
+>
+	{#if !show}
+		Read More
+	{:else}
+		Close
+	{/if}
+</button>
+
+{#if show}
+	<slot />
+{/if}
+
+<style>
+	.readmore {
+		font-size: 1rem;
+		color: grey;
+		cursor: pointer;
+		background: none;
+		border: none;
+		outline: none;
+	}
+</style>
